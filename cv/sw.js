8419cab6ffb36d148d95aa51d3531db22cdafc72
Briand IDOSSOU <idossoubriand@gmail.com>
2016-06-05 17:56:20 +0000

    Tests for offline first

---
+++ cv/sw.js
@@ -0,0 +1,36 @@
+
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open("cv-v1").then(function(cache){
+			return cache.addAll([
+				'/',
+				'assets/css/styles.css',
+				'assets/images/profile.png',
+				'assets/images/projects/project-1.png',
+				'assets/images/projects/project-2.png',
+				'assets/js/main.js',
+				'librairies/bootstrap/css/bootstrap.min.css',
+				'librairies/font-awesome/css/font-awesome.css',
+				'librairies/jquery-1.11.2.min.js',
+				'librairies/jquery-migrate-1.2.1.min.js',
+				'librairies/bootstrap/js/bootstrap.min.js',
+				'assets/js/main.js',
+				'favicon.ico',
+			]);
+		})
+	);
+});
+
+
+	self.addEventListener('fetch', function(event){
+
+	console.log("Fetch response");
+
+		event.respondWith(
+			caches.match(event.request).then(function(response){
+			if(response) return response;
+			return fetch(event.request);
+			
+			})
+		);
+	});
